9f81294261d1990a997c3d3a242f4eb052379fb7
Sampa Nayak <sampanayak987@gmail.com>
2026-01-29 20:39:22 +0530

    msg

---
+++ JavaScript/Day1/day1.js
@@ -0,0 +1,12 @@
+console.log("Welcome JavaScript !")
+
+let a=19;
+console.log("Value of a is : "+a)
+
+//when you decalred a variable but don't assign any value to it.
+let x;
+console.log(x)  //undefined
+
+//when you declared a variable and assign value of null. Internal ansence of any value.
+let y = null;
+console.log(y) //null
